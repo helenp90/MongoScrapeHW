5cd10c270cc3c81e3a17c748c635f68f29dbf70a
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2018-02-11 16:25:19 -0500

    updated the app.js for recipes

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/recipe", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#recipes").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
   });
   // Whenever someone clicks a p tag
@@ -15,7 +15,7 @@ $.getJSON("/articles", function(data) {
     // Now make an ajax call for the Article
     $.ajax({
       method: "GET",
-      url: "/articles/" + thisId
+      url: "/recipe/" + thisId
     })
       // With that done, add the note information to the page
       .then(function(data) {
@@ -44,7 +44,7 @@ $.getJSON("/articles", function(data) {
     // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
       method: "POST",
-      url: "/articles/" + thisId,
+      url: "/recipes/" + thisId,
       data: {
         // Value taken from title input
         title: $("#titleinput").val(),
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Dependencies================
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
@@ -7,6 +9,8 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
+
+//=============================
 var PORT = 3008;
 // Initialize Express
 var app = express();
@@ -16,9 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect("mongodb://localhost:27017/scrapedb", {
   useMongoClient: true
 });
 // Routes
@@ -40,10 +45,10 @@ app.get("/scrape", function(req, res) {
         .children("a")
         .attr("href");
       // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
+      db.Recipe.create(result)
+        .then(function(dbRecipe) {
           // View the added result in the console
-          console.log(dbArticle);
+          console.log(dbRecipe);
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
@@ -55,12 +60,12 @@ app.get("/scrape", function(req, res) {
   });
 });
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/recipes", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
+  db.Recipe.find({})
+    .then(function(dbRecipe) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.json(dbRecipe);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -68,14 +73,14 @@ app.get("/articles", function(req, res) {
     });
 });
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/recipe/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  db.Recipe.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function(dbRecipe) {
       // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
+      res.json(dbRecipe);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
@@ -83,18 +88,18 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/recipe/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Recipe.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function(dbRecipe) {
       // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
+      res.json(dbRecipe);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
